1eeaeb9eac9d45e0463069b0c9981d4884699e30
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-27 14:35:41 -0500

    changed title of html document

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -55,7 +55,7 @@ function generateHtml(fullTeam){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <title>Document</title>
+        <title>Team Generator</title>
     </head>
     <body>
         <div class="jumbotron jumbotron-fluid bg-primary">
